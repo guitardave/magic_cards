8db0220c304b9b5258dd926172cadc159c29dabf
dcrandell <david@onholdwizard.com>
2024-09-19 09:09:24 -0700

    add heroku - pretty sure that's why resources from /static are not loading

--- mtg/settings.py
+++ mtg/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,3 +148,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
